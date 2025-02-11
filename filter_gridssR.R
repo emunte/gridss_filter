7ab3dfca9cd4476a5fa9630f6385f2b84d0838c4
emunte <elisabetmunte@gmail.com>
2025-02-11 17:12:42 +0100

    reoder columns

--- filter_gridssR.R
+++ filter_gridssR.R
@@ -27,7 +27,7 @@ invisible(sapply(packages, install_if_missing))
 option_list <- list(
   optparse::make_option(c("-i", "--input"), type="character", default="",
                         help="Folder with merge results", metavar="character"),
-  optparse::make_option(c("-d", "--bedFile"), type="character", default="",
+  optparse::make_option(c("-b", "--bedFile"), type="character", default="",
                        help="path to the bed file", metavar="character"),
   optparse::make_option(c("-p", "--params"), type="character", default="params.yaml",
                         help="Yaml file with all the specifications", metavar="integer"),
@@ -38,22 +38,25 @@ option_list <- list(
 opt_parser <- optparse::OptionParser(option_list=option_list);
 # Load params
 args <- optparse::parse_args(opt_parser)
-input <- args$txt
+input <- args$input
 output <- args$output
 bedFile <- args$bedFile %>% rtracklayer::import()
-params <- yaml::yaml.load(args$params)
+params <- yaml::yaml.load_file(args$params)
 
+frequency <- params$frequency
+gt.AF <- params$gt_AF
+highly.similar <- params$similar
+minimum.length <- params$minimumLength
 
 
 # 3. Load data
-
+print("Loading data")
 #Get the two dataframes (merge_gridss_vcf output)
-all.one.break <- list.files(input, pattern="merge_gridss_one_break.txt", recursive=TRUE, full.names=TRUE) %>%
+all.one.break <- list.files(path=input, pattern="merge_gridss_one_break.txt$", recursive=TRUE, full.names=TRUE) %>%
   read.delim(header=TRUE, sep="\t") %>%
   dplyr::mutate(end=POS)
 
-
-all.two.break <- list.files(input, pattern="merge_gridss_two_break_same_chrom.txt", recursive=TRUE, full.names=TRUE) %>%
+all.two.break <- list.files(input, pattern="merge_gridss_two_break.txt", recursive=TRUE, full.names=TRUE) %>%
   read.delim(header=TRUE, sep="\t") %>%  #reorder
   dplyr::mutate(
     chrom1=ifelse(POS.y < POS.x, CHROM.y, CHROM.x),
@@ -64,16 +67,19 @@ all.two.break <- list.files(input, pattern="merge_gridss_two_break_same_chrom.tx
     alt2= ifelse(POS.y>POS.x, ALT.y, ALT.x))
 
 
-
 # 4. Pipeline----
 #register number of initial variants---
 filters.variants <- data.frame(type=c("one_break", "two_break"),totals=c(nrow(all.one.break), nrow(all.two.break)) )
-
+print ("Filtering variants")
+print (paste("Parameters used:",
+"frequency ≤", frequency,
+"gt_AF ≥", gt.AF,
+"similar ≤", highly.similar,
+"minimumLength ≥", minimum.length))
 
 
 ## A) Frequency filters----
-#Delete variants found in >10 samples
-
+#Delete variants found in >=10 samples
 #one-break
 count.obs <-  all.one.break %>%
   dplyr::count(CHROM, POS, ALT, name = "n") #count same variant (CHROM; POS; ALT)
@@ -84,7 +90,7 @@ overlaps.one.sample.count <- all.one.break %>%  #merge results
   dplyr::left_join(count.obs2, by = c("CHROM", "POS")) %>%
   dplyr::left_join(count.obs, by=c("CHROM", "POS", "ALT"))%>%
   dplyr::mutate(start = POS) %>%
-  dplyr::filter(n <= frequency.filter) %>% #filter
+  dplyr::filter(n <= frequency) %>% #filter
   dplyr::relocate(CHROM, start, end, run, sample, genes.interest, .before = POS)
 
 #two-breaks
@@ -96,7 +102,7 @@ count.obs.two2 <- all.two.break %>%
 overlaps.two.sample.count <- all.two.break %>%
   dplyr::left_join(count.obs.two2, by = c("chrom1", "chrom2", "start", "end")) %>%
   dplyr::left_join(count.obs.two, by=c("chrom1", "chrom2", "start", "end", "alt1", "alt2")) %>%
-  dplyr::filter(n <= frequency.filter) %>% #filter
+  dplyr::filter(n <= frequency) %>% #filter
   dplyr::relocate(run, sample, genes.interest, chrom1,chrom2, start, end, .before = EVENT)
 
 #Store how many variants remain
@@ -177,29 +183,35 @@ extract_gene_name <- function(gene_column) {
 filter_genes_of_interest <- function(df) {
   if ("gene.x" %in% colnames(df) & "gene.y" %in% colnames(df)) {
     # Two-breakend case: Extract gene names from `gene.x` and `gene.y`
-    df <- df %>%
-      mutate(
-        gene.x.name = extract_gene_name(gene.x),
-        gene.y.name = extract_gene_name(gene.y)
-      )
+    # df <- df %>%
+    #   dplyr::mutate(
+    #     gene.x.name = extract_gene_name(gene.x),
+    #     gene.y.name = extract_gene_name(gene.y))
+
+    df.filtered <- df %>%
+          filter(genes.interest == "ALL_BEDFILE" |
+                   stringr::str_detect(genes.interest, gene.x)|
+                   stringr::str_detect(genes.interest, gene.y))
+
   } else if ("gene" %in% colnames(df)) {
     # One-breakend case: Extract gene name from `gene`
-    df <- df %>%
-      mutate(gene.name = extract_gene_name(gene))
-  } else {
-    stop("Error: No gene columns found (expected 'gene.x' and 'gene.y' for two breakends or 'gene' for one breakend).")
+    # df <- df %>%
+    # mutate(gene.name = extract_gene_name(gene))
+    df.filtered <- df %>%
+      filter(genes.interest == "ALL_BEDFILE" |
+               stringr::str_detect(genes.interest, gene))
   }
 
   # Filter data independently for each sample
-  df.filtered <- df %>%
-    group_by(sample) %>%  # Group by sample to apply different filters per sample
-    filter(
-      unique(genes.interest) == "ALL_BEDFILE" |
-        any(across(any_of(c("gene.x.name", "gene.y.name", "gene.name")),
-                                                            ~ . %in% unlist(strsplit(unique(genes.interest), ",\\s*"))
-      ))
-    ) %>%
-    ungroup()  # Remove grouping
+  # df.filtered <- df %>%
+  #   group_by(sample) %>%  # Group by sample to apply different filters per sample
+  #   filter(
+  #     unique(genes.interest) == "ALL_BEDFILE" |
+  #       any(across(any_of(c("gene.x.name", "gene.y.name", "gene.name")),
+  #                                                           ~ . %in% unlist(strsplit(unique(genes.interest), ",\\s*"))
+  #     ))
+  #   ) %>%
+  #   ungroup()  # Remove grouping
   return(df.filtered)  # Return the filtered dataset
 }
 
@@ -209,16 +221,16 @@ ov.two.cascade <- filter_genes_of_interest(overlaps.two)
 filters.variants <- filters.variants %>%
   dplyr::mutate(phenotype=c(nrow(ov.one.cascade), nrow(ov.two.cascade)))
 
-#C) Filter by gt_AF ----
+#C) Filter by gt.AF ----
 AF.one <- ov.one.cascade %>%
-  dplyr::filter(gt_AF >= args$gt_AF)
+  dplyr::filter(gt_AF >= gt.AF)
 
 AF.two <- ov.two.cascade %>%
-  dplyr::filter(gt_AF.x >= args$gt_AF &
-                  gt_AF.y >= args$gt_AF)
+  dplyr::filter(gt_AF.x >= gt.AF &
+                  gt_AF.y >= gt.AF)
 
 filters.variants <- filters.variants %>%
-  dplyr::mutate(gt_AF=c(nrow(AF.one), nrow(AF.two)))
+  dplyr::mutate(gt.AF=c(nrow(AF.one), nrow(AF.two)))
 
 
 #D) Delete variants in repetitive regions
@@ -242,8 +254,10 @@ filters.variants <- filters.variants %>%
 
 
 # F) For one break get only MEI variants
+
 one.b.definitive <- ov.one.hot.spot %>%
-  dplyr::filter(INSRMRC %in% c("SINE", "LINE"))
+  dplyr::filter(stringr::str_detect(INSRMRC, "LINE|SINE|LTR")) %>%
+  dplyr::relocate(run, sample, chr, POS, gene, gt_AF,INSRMRC, INSRMRT, n, n2, genes.interest, .before=ID )
 
 #E) For two breakends only consider variants that are bigger than X (if located at the same chr)
 
@@ -253,11 +267,20 @@ count_ATCG <- function(seq_vector) {
   return(lengths)
 }
 two.b.definitive <- ov.two.hot.spot %>%
-  dplyr::filter(CHROM.x!=CHROM.y | CHROM.x==CHROM.y & (end-start >= minimumLength | (end-start ==1 & count_ATCG(ALT.x) >= minimumLegth)))
+  dplyr::filter(CHROM.x!=CHROM.y | CHROM.x==CHROM.y & (end-start >= minimum.length | (end-start ==1 & count_ATCG(ALT.x) >= minimum.length))) %>%
+  dplyr::relocate(run, sample, CHROM.x, CHROM.y, POS.x, POS.y, gene.x, gene.y, gt_AF.x, gt_AF.y,  n, n2, genes.interest, .before=EVENT )
+
+
+#Save files
+filtered.dir <- file.path(args$output, paste0("filtered_variants"))
+dir.create(filtered.dir)
+openxlsx::write.xlsx(one.b.definitive, file.path(filtered.dir, "filtered_variants_one_break.xlsx"))
+openxlsx::write.xlsx(two.b.definitive, file.path(filtered.dir, "filtered_variants_two_break.xlsx"))
 
 
 
 #Plot diagram
+print("Printing results")
 workflow_filtering <- function(what, filters, one.def, two.def){
 wf<- grViz(paste("
 digraph flow {
@@ -268,23 +291,23 @@ digraph flow {
   # Nodes
   Total_Variants [label = 'Total", what, "breakend", filters.variants$totals," variants', shape = box, style = filled, fillcolor = lightgrey]
 
-  Filter1 [label = 'Present in \n ≤", args$frequency, "samples?', shape = diamond, fillcolor = white]
+  Filter1 [label = 'Present in \n ≤", frequency, "samples?', shape = diamond, fillcolor = white]
   Pass1 [label ='",  filters.variants$nsample,"variants', fillcolor = lightblue]
-  Fail1 [label = '", filters.variants$totals-filters.variants$totals, "variants', fillcolor = lightcoral]
+  Fail1 [label = '", filters.variants$totals-filters.variants$nsample, "variants', fillcolor = lightcoral]
 
   Filter2 [label = 'Phenotype genes?', shape = diamond, fillcolor = white]
   Pass2 [label = '",  filters.variants$phenotype,"variants', fillcolor = lightblue]
   Fail2 [label = '",  filters.variants$nsample-filters.variants$phenotype," variants', fillcolor = lightcoral]
 
-  Filter3 [label = 'VAF ≥", args$gt_AF, "', shape = diamond, fillcolor = white]
-  Pass3 [label = '",  filters.variants$gt_AF,"variants', fillcolor = lightblue]
-  Fail3 [label = '",  filters.variants$phenotype- filters.variants$gt_AF,"variants', fillcolor = lightcoral]
+  Filter3 [label = 'VAF ≥", gt.AF, "', shape = diamond, fillcolor = white]
+  Pass3 [label = '",  filters.variants$gt.AF,"variants', fillcolor = lightblue]
+  Fail3 [label = '",  filters.variants$phenotype- filters.variants$gt.AF,"variants', fillcolor = lightcoral]
 
   Filter4 [label = 'outside a simple \n repeat  or low \n complexity region', shape = diamond, fillcolor = white]
   Pass4 [label = '",  filters.variants$simple,"variants', fillcolor = lightblue]
-  Fail4 [label = '",  filters.variants$gt_AF- filters.variants$simple,"variants', fillcolor = lightcoral]
+  Fail4 [label = '",  filters.variants$gt.AF- filters.variants$simple,"variants', fillcolor = lightcoral]
 
-  Filter5 [label = 'highly similar \n variant ≤", args$similar, " ', shape = diamond, fillcolor = white]
+  Filter5 [label = 'highly similar \n variant ≤", highly.similar, " ', shape = diamond, fillcolor = white]
   Pass5 [label = '",  filters.variants$similar, "variants', fillcolor = lightblue]
   Fail5 [label = '",  filters.variants$simple- filters.variants$similar,"variants', fillcolor = lightcoral]",
 
@@ -293,7 +316,7 @@ digraph flow {
     Pass6 [label = '",  nrow(one.b.definitive),"variants', fillcolor = lightblue]
     Fail6 [label = '",  filters.variants$similar- nrow(one.b.definitive),"variants', fillcolor = lightcoral]")
   }else{
-    paste("Filter6 [label = 'minimum length \n ≥", args$minimumLength, "bp', shape = diamond, fillcolor = white]
+    paste("Filter6 [label = 'minimum length \n ≥", minimum.length, "bp', shape = diamond, fillcolor = white]
     Pass6 [label = '",  nrow(two.b.definitive),"variants', fillcolor = lightblue]
     Fail6 [label = '",  filters.variants$similar- nrow(two.b.definitive),"variants', fillcolor = lightcoral]")
   },
