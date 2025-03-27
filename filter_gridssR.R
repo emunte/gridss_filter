dc9f68dac22ab9ea48341ff6963e3026d0d37237
emunte <elisabetmunte@gmail.com>
2025-03-27 17:15:20 +0100

    merge intervals MEI

--- filter_gridssR.R
+++ filter_gridssR.R
@@ -14,7 +14,7 @@ install_if_missing <- function(pkg) {
 }
 
 #List of libraries
-packages <- c("dplyr", "optparse", "stringr", "GenomicRanges", "yaml", "DiagrammeR", "DiagrammeRsvg", "rsvg")
+packages <- c("dplyr", "optparse", "stringr", "GenomicRanges", "yaml", "DiagrammeR", "DiagrammeRsvg", "rsvg", "assertthat")
 
 
 #Apply
@@ -29,18 +29,30 @@ option_list <- list(
                         help="Folder with merge results", metavar="character"),
   optparse::make_option(c("-b", "--bedFile"), type="character", default="",
                        help="path to the bed file", metavar="character"),
+  #optparse::make_option(c("-d", "--databaseExternal"), type=logical, default=FALSE,
+  #                      help="Do you want to provide an external db?", metavar="integer"),
+  #optparse::make_option(c("-f", "--externalFrequency1"), type="character", default="",
+  #                      help="RDA object with the external frequencies one break (see readme)"),
+  #optparse::make_option(c("-t", "--internalFrequency2"), type="character", default="",
+  #                      help="RDA object with the internal frequencies two breaks (see readme)"),
   optparse::make_option(c("-p", "--params"), type="character", default="params.yaml",
-                        help="Yaml file with all the specifications", metavar="integer"),
+                        help="Params yaml", metavar="character"),
+  optparse::make_option(c("-m", "--mergeVariants"), type="numeric", default=4,
+                        help="Numeric threshold (in bp) for merging one-break variants. If set to 0, no merging is performed", metavar="character"),
   optparse::make_option(c("-o", "--output"), type="character", default="",
-                        help="Path to the output directory. Results will be stored in this folder", metavar="character")
+                        help="Path to the output directory. Results will be stored in this folder", metavar="character"),
+  optparse::make_option(c("-n", "--name"), type="character", default="",
+                        help="Name the output", metavar="character")
 )
 
 opt_parser <- optparse::OptionParser(option_list=option_list);
 # Load params
 args <- optparse::parse_args(opt_parser)
 input <- args$input
+merge.vars <- args$mergeVariants
 output <- args$output
 bedFile <- args$bedFile %>% rtracklayer::import()
+name <- args$name
 params <- yaml::yaml.load_file(args$params)
 
 frequency <- params$frequency
@@ -54,7 +66,8 @@ print("Loading data")
 #Get the two dataframes (merge_gridss_vcf output)
 all.one.break <- list.files(path=input, pattern="merge_gridss_one_break.txt$", recursive=TRUE, full.names=TRUE) %>%
   read.delim(header=TRUE, sep="\t") %>%
-  dplyr::mutate(end=POS)
+  dplyr::mutate(end=POS) %>% # f) Assign a unique ID to each row
+  mutate(row.id = row_number())
 
 all.two.break <- list.files(input, pattern="merge_gridss_two_break.txt", recursive=TRUE, full.names=TRUE) %>%
   read.delim(header=TRUE, sep="\t") %>%  #reorder
@@ -72,9 +85,9 @@ all.two.break <- list.files(input, pattern="merge_gridss_two_break.txt", recursi
 filters.variants <- data.frame(type=c("one_break", "two_break"),totals=c(nrow(all.one.break), nrow(all.two.break)) )
 print ("Filtering variants")
 print (paste("Parameters used:",
-"frequency ≤", frequency,
+"frequency <", frequency,
 "gt_AF ≥", gt.AF,
-"similar ≤", highly.similar,
+"similar <", highly.similar,
 "minimumLength ≥", minimum.length))
 
 
@@ -86,11 +99,16 @@ count.obs <-  all.one.break %>%
 count.obs2 <-  all.one.break %>%
   dplyr::count(CHROM, POS,  name = "n2") #count similar variants (CHROM; POS)
 
+#Load intervals MEI
+source("intervals_MEI.R")
+count.obs3 <- intervals_INSRMRT(all.one.break, merge.vars) #count same variants (CHROM; POS (rang); INSRMRT)
+
 overlaps.one.sample.count <- all.one.break %>%  #merge results
   dplyr::left_join(count.obs2, by = c("CHROM", "POS")) %>%
-  dplyr::left_join(count.obs, by=c("CHROM", "POS", "ALT"))%>%
+  dplyr::left_join(count.obs, by = c("CHROM", "POS", "ALT"))%>%
+  dplyr::left_join(count.obs3 %>% dplyr::select("row.id", "cluster.id", "n.variants"), by=c("row.id")) %>%
   dplyr::mutate(start = POS) %>%
-  dplyr::filter(n <= frequency) %>% #filter
+  dplyr::filter(n < frequency) %>% #filter
   dplyr::relocate(CHROM, start, end, run, sample, genes.interest, .before = POS)
 
 #two-breaks
@@ -102,7 +120,7 @@ count.obs.two2 <- all.two.break %>%
 overlaps.two.sample.count <- all.two.break %>%
   dplyr::left_join(count.obs.two2, by = c("chrom1", "chrom2", "start", "end")) %>%
   dplyr::left_join(count.obs.two, by=c("chrom1", "chrom2", "start", "end", "alt1", "alt2")) %>%
-  dplyr::filter(n <= frequency) %>% #filter
+  dplyr::filter(n < frequency) %>% #filter
   dplyr::relocate(run, sample, genes.interest, chrom1,chrom2, start, end, .before = EVENT)
 
 #Store how many variants remain
@@ -175,19 +193,9 @@ overlaps.two<- merge(all.events,  overlaps.two.sample.count , by=c(c("EVENT", "M
 
 # B.2) Only consider genes of interest (if you want to analyze all genes, put ALL_BEDFILE in the colunm genes.interest.
 
-# Function to extract gene names from a column containing "ENSTxxx|GENE"
-extract_gene_name <- function(gene_column) {
-  stringr::str_extract(gene_column, "(?<=\\|)[^|]+")  # Agafa el text després de "|"
-}
 # Function to filter events based on genes of interest  # Extract gene names from `gene.x` and `gene.y` columns
 filter_genes_of_interest <- function(df) {
   if ("gene.x" %in% colnames(df) & "gene.y" %in% colnames(df)) {
-    # Two-breakend case: Extract gene names from `gene.x` and `gene.y`
-    # df <- df %>%
-    #   dplyr::mutate(
-    #     gene.x.name = extract_gene_name(gene.x),
-    #     gene.y.name = extract_gene_name(gene.y))
-
     df.filtered <- df %>%
           filter(genes.interest == "ALL_BEDFILE" |
                    stringr::str_detect(genes.interest, gene.x)|
@@ -195,23 +203,11 @@ filter_genes_of_interest <- function(df) {
 
   } else if ("gene" %in% colnames(df)) {
     # One-breakend case: Extract gene name from `gene`
-    # df <- df %>%
-    # mutate(gene.name = extract_gene_name(gene))
     df.filtered <- df %>%
       filter(genes.interest == "ALL_BEDFILE" |
                stringr::str_detect(genes.interest, gene))
   }
 
-  # Filter data independently for each sample
-  # df.filtered <- df %>%
-  #   group_by(sample) %>%  # Group by sample to apply different filters per sample
-  #   filter(
-  #     unique(genes.interest) == "ALL_BEDFILE" |
-  #       any(across(any_of(c("gene.x.name", "gene.y.name", "gene.name")),
-  #                                                           ~ . %in% unlist(strsplit(unique(genes.interest), ",\\s*"))
-  #     ))
-  #   ) %>%
-  #   ungroup()  # Remove grouping
   return(df.filtered)  # Return the filtered dataset
 }
 
@@ -244,10 +240,21 @@ filters.variants <- filters.variants %>%
 
 #E) Delete highly similar variants (CHR, POS)
 ov.one.hot.spot <- ov.one.no.repeats %>%
-  dplyr::filter(n2<=highly.similar)
+  mutate(INSRMRC = tidyr::replace_na(INSRMRC, "")) %>%
+  dplyr::filter(
+    (
+      !stringr::str_detect(INSRMRC, "LINE|SINE|LTR") &
+        n2 < highly.similar
+    ) |
+      (
+        stringr::str_detect(INSRMRC, "LINE|SINE|LTR") &
+          n2 < highly.similar &
+          n.variants < highly.similar
+      )
+  )
 
 ov.two.hot.spot <- ov.two.no.repeats %>%
-  dplyr::filter(n2<=highly.similar)
+  dplyr::filter(n2<highly.similar)
 
 filters.variants <- filters.variants %>%
   dplyr::mutate(similar=c(nrow(ov.one.hot.spot), nrow(ov.two.hot.spot)))
@@ -257,12 +264,12 @@ filters.variants <- filters.variants %>%
 
 one.b.definitive <- ov.one.hot.spot %>%
   dplyr::filter(stringr::str_detect(INSRMRC, "LINE|SINE|LTR")) %>%
-  dplyr::relocate(run, sample, chr, POS, gene, gt_AF,INSRMRC, INSRMRT, n, n2, genes.interest, .before=ID )
+  dplyr::relocate(run, sample, chr, POS, gene, gt_AF,INSRMRC, INSRMRT, n, n2, n.variants, genes.interest, .before=ID )
 
 #E) For two breakends only consider variants that are bigger than X (if located at the same chr)
 
 count_ATCG <- function(seq_vector) {
-  extracted <- str_extract_all(seq_vector, "[ATCG]+")  # Extreu només A, T, C, G
+  extracted <- stringr::str_extract_all(seq_vector, "[ATCG]+")  # Extreu només A, T, C, G
   lengths <- sapply(extracted, nchar)  # Compta quants nucleòtids hi ha en cada entrada
   return(lengths)
 }
@@ -273,15 +280,15 @@ two.b.definitive <- ov.two.hot.spot %>%
 
 #Save files
 filtered.dir <- file.path(args$output, paste0("filtered_variants"))
-dir.create(filtered.dir)
-openxlsx::write.xlsx(one.b.definitive, file.path(filtered.dir, "filtered_variants_one_break.xlsx"))
-openxlsx::write.xlsx(two.b.definitive, file.path(filtered.dir, "filtered_variants_two_break.xlsx"))
+dir.create(filtered.dir, showWarnings = FALSE)
+openxlsx::write.xlsx(one.b.definitive, file.path(filtered.dir, paste(Sys.Date(),  name,"filtered_variants_one_break.xlsx", sep = "_")))
+openxlsx::write.xlsx(two.b.definitive, file.path(filtered.dir, paste(Sys.Date(),  name,"filtered_variants_two_break.xlsx", sep = "_")))
 
 
 
 #Plot diagram
 print("Printing results")
-workflow_filtering <- function(what, filters, one.def, two.def){
+workflow_filtering <- function(what, filters, definitive){
 wf<- grViz(paste("
 digraph flow {
   graph [layout = dot, rankdir = TB]
@@ -289,38 +296,35 @@ digraph flow {
   node [shape = rectangle, style = filled, fillcolor = white, fontname = Helvetica, fontsize = 12]
 
   # Nodes
-  Total_Variants [label = 'Total", what, "breakend", filters.variants$totals," variants', shape = box, style = filled, fillcolor = lightgrey]
+  Total_Variants [label = 'Total", what, "breakend", filters$totals," variants', shape = box, style = filled, fillcolor = lightgrey]
 
-  Filter1 [label = 'Present in \n ≤", frequency, "samples?', shape = diamond, fillcolor = white]
-  Pass1 [label ='",  filters.variants$nsample,"variants', fillcolor = lightblue]
-  Fail1 [label = '", filters.variants$totals-filters.variants$nsample, "variants', fillcolor = lightcoral]
+  Filter1 [label = 'Present in \n <", frequency, "samples?', shape = diamond, fillcolor = white]
+  Pass1 [label ='",  filters$nsample,"variants', fillcolor = lightblue]
+  Fail1 [label = '", filters$totals-filters$nsample, "variants', fillcolor = lightcoral]
 
   Filter2 [label = 'Phenotype genes?', shape = diamond, fillcolor = white]
-  Pass2 [label = '",  filters.variants$phenotype,"variants', fillcolor = lightblue]
-  Fail2 [label = '",  filters.variants$nsample-filters.variants$phenotype," variants', fillcolor = lightcoral]
+  Pass2 [label = '",  filters$phenotype,"variants', fillcolor = lightblue]
+  Fail2 [label = '",  filters$nsample-filters$phenotype," variants', fillcolor = lightcoral]
 
   Filter3 [label = 'VAF ≥", gt.AF, "', shape = diamond, fillcolor = white]
-  Pass3 [label = '",  filters.variants$gt.AF,"variants', fillcolor = lightblue]
-  Fail3 [label = '",  filters.variants$phenotype- filters.variants$gt.AF,"variants', fillcolor = lightcoral]
+  Pass3 [label = '",  filters$gt.AF,"variants', fillcolor = lightblue]
+  Fail3 [label = '",  filters$phenotype- filters$gt.AF,"variants', fillcolor = lightcoral]
 
   Filter4 [label = 'outside a simple \n repeat  or low \n complexity region', shape = diamond, fillcolor = white]
-  Pass4 [label = '",  filters.variants$simple,"variants', fillcolor = lightblue]
-  Fail4 [label = '",  filters.variants$gt.AF- filters.variants$simple,"variants', fillcolor = lightcoral]
+  Pass4 [label = '",  filters$simple,"variants', fillcolor = lightblue]
+  Fail4 [label = '",  filters$gt.AF- filters$simple,"variants', fillcolor = lightcoral]
 
   Filter5 [label = 'highly similar \n variant ≤", highly.similar, " ', shape = diamond, fillcolor = white]
-  Pass5 [label = '",  filters.variants$similar, "variants', fillcolor = lightblue]
-  Fail5 [label = '",  filters.variants$simple- filters.variants$similar,"variants', fillcolor = lightcoral]",
+  Pass5 [label = '",  filters$similar, "variants', fillcolor = lightblue]
+  Fail5 [label = '",  filters$simple- filters$similar,"variants', fillcolor = lightcoral]",
 
   if(what=="one"){
-    paste("Filter6 [label = 'Transposable \n elements', shape = diamond, fillcolor = white]
-    Pass6 [label = '",  nrow(one.b.definitive),"variants', fillcolor = lightblue]
-    Fail6 [label = '",  filters.variants$similar- nrow(one.b.definitive),"variants', fillcolor = lightcoral]")
-  }else{
-    paste("Filter6 [label = 'minimum length \n ≥", minimum.length, "bp', shape = diamond, fillcolor = white]
-    Pass6 [label = '",  nrow(two.b.definitive),"variants', fillcolor = lightblue]
-    Fail6 [label = '",  filters.variants$similar- nrow(two.b.definitive),"variants', fillcolor = lightcoral]")
-  },
-  "
+  paste("Filter6 [label = 'Transposable \n elements', shape = diamond, fillcolor = white]")
+  }else {
+  paste("Filter6 [label = 'Minimum length \n ≥", minimum.length, "bp', shape = diamond, fillcolor = white]")},
+  "Pass6 [label = '",  nrow(definitive),"variants', fillcolor = lightblue]
+  Fail6 [label = '",  filters$similar- nrow(definitive),"variants', fillcolor = lightcoral]
+
   # Edges
   Total_Variants -> Filter1
   Filter1 -> Pass1 [label = 'Yes']
@@ -350,9 +354,9 @@ return(wf)
 }
 
 plots.dir <- file.path(args$output, "plots")
-dir.create(plots.dir)
-one.wf <- workflow_filtering("one", filters.variants[1,], one.b.definitive, two.b.definitive)
-two.wf <- workflow_filtering("two", filters.variants[2,], one.b.definitive, two.b.definitive)
+dir.create(plots.dir, showWarnings = FALSE)
+one.wf <- workflow_filtering("one", filters.variants[1,], one.b.definitive)
+two.wf <- workflow_filtering("two", filters.variants[2,], two.b.definitive)
 
 
 # convert to SVG
@@ -360,8 +364,8 @@ svg.one <- DiagrammeRsvg::export_svg(one.wf)
 svg.two <- DiagrammeRsvg::export_svg(two.wf)
 
 # Save as PNG
-rsvg_png(charToRaw(svg.one), file = file.path(plots.dir, "workflow_diagram_one.png"))
-rsvg_png(charToRaw(svg.two), file = file.path(plots.dir, "workflow_diagram_two.png"))
+rsvg_png(charToRaw(svg.one), file = file.path(plots.dir, paste(Sys.Date(), name, "workflow_diagram_one.png", sep = "_")))
+rsvg_png(charToRaw(svg.two), file = file.path(plots.dir, paste(Sys.Date(), name, "workflow_diagram_two.png", sep = "_")))
 
 
 
